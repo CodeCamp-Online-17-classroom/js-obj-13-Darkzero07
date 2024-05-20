ea9a0a76b1bec63b690891fbc3e31e60efcd8a41
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-20 22:06:09 +0700

    js lab 59

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// write code here
+const product1 = { name: 'Coke', price: 18, size: '500mL' };
+
+const product2 = product1;
+product2.name = 'Pepsi';
+product2.price = 19;
+
+console.log(product1);                     // { name: 'Pepsi', price: 19, size: '500mL' }
+console.log(product2);                   // { name: 'Pepsi', price: 19, size: '500mL' }
+console.log(product1 === product2);     // true
+// because product 2 equal to product 1 
